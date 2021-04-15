f8b25bb85dde77f61c06869d4324d168f64d6ca3
Poppy Jennings <popjen@hotmail.com>
2021-04-15 02:30:20 +0200

    removed carousel added cards

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,7 +20,7 @@ document.addEventListener('turbolinks:load', () => {
   loadDynamicFooterText();
 });
 
-$('.carousel').carousel()
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
